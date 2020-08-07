144066cde0de0aa237a4b41e433e1a347e3f3986
Ty Hunt <jtmhunt22@gmail.com>
2020-08-07 08:58:06 -0600

    just added some comments

--- public/assets/js/savedcampground.js
+++ public/assets/js/savedcampground.js
@@ -1,6 +1,24 @@
-// ! have to use this to use the api keys;
-// require("dotenv").config();
+//  all of our code goes inside of this
+$(document).ready(function() {
+  // todo: get all of our html variables
 
-// ! api key variable
-// ! this is how you use the api key
-// const apiKey = process.env.API_KEY;
+  // todo: create a new campsite and send it to our database
+  // !  const newCampSite = {
+  //   it will look something like this
+  //  !     name: nameInput.val().trim(),
+  //   !    location: location.val().trim(),
+  //    !   description: description.val().trim(),
+  // ! }
+
+  // todo: create a function to get all of our campsites
+
+  // todo: create a function to update a post
+
+  //todo: create a function where we create a new campsite
+
+  //todo: create a function to delete a post
+
+  // ? create the html dynamically to send the campsites and the new campsites
+
+});
+
