f3ef7a03ef9208d1da19e6c03a29a4f395ff686e
CodingwithJess <thejessiebess@gmail.com>
2020-08-07 20:17:17 -0600

    updated styling on saved and wishlist pages

--- public/assets/js/savedcampground.js
+++ public/assets/js/savedcampground.js
@@ -2,38 +2,39 @@
 // $(document).ready(function() {
 //   // todo: get all of our html variables
 
-  // todo: create a new campsite and send it to our database
-  $(".create-form").on("submit", function(event){
-    event.preventDefault();
+// todo: create a new campsite and send it to our database
+$(".create-form").on("submit", function (event) {
+  event.preventDefault();
 
-    const newCampSite = {
-      // it will look something like this
-      name: $(".campName").val().trim(),
-      location: $(".campLocation").val().trim(),
-      description: campDescription.val().trim(),
-      rating: rating.val().trim(),
-    };
-    $.ajax("api/campsites", {
-      type: "POST",
-      data: newCampSite
-    }).then(function(){
-      console.log("created new campsite");
-      location.reload();
-    });
+  const newCampSite = {
+    // it will look something like this
+    name: $(".campName").val().trim(),
+    location: $(".campLocation").val().trim(),
+    description: campDescription.val().trim(),
+    rating: rating.val().trim(),
+  };
+  $.ajax("api/campsites", {
+    type: "POST",
+    data: newCampSite,
+  }).then(function () {
+    console.log("created new campsite");
+    location.reload();
   });
+});
 
-  //todo: create a function where we create a new campsite
+//todo: create a function where we create a new campsite
 
-  //todo: create a function to delete a post
-  $(".delete-campsite").on("click", function(event){
-    event.preventDefault();
+//todo: create a function to delete a post
+$(".delete-campsite").on("click", function (event) {
+  event.preventDefault();
 
-    const id = $(this).data("id");
-    $.ajax("/api/campsites" + id, {
-      type: "DELETE",
-    }).then( function() {
-      console.log("deleted campsite", id);
-      location.reload();
-    });
+  const id = $(this).data("id");
+  $.ajax("/api/campsites" + id, {
+    type: "DELETE",
+  }).then(function () {
+    console.log("deleted campsite", id);
+    location.reload();
   });
+});
+
 
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,92 +3,79 @@
 // Dependencies
 
 //Require model
-const {
-    Campsite
-} = require("../models");
+const { Campsite } = require("../models");
 
 //Routes
-//GET ROUTES-This shows the data from the datebase on the page
+//GET ROUTES-This shows the data from the database on the page
 module.exports = function (app) {
-    // GET route for getting all posts
-
-    app.get("/api/campsites/", function (req, res) {
-        Campsite.findAll({})
-            .then(function (Campsite) {
-                res.json(Campsite);
-            });
-
+  // GET route for getting all posts
+  app.get("/api/campsites/", function (req, res) {
+    Campsite.findAll({}).then(function (Campsite) {
+      res.json(Campsite);
     });
+  });
 
-    app.get("/api/campsites/rating/:rating", function (req, res) {
-        Campsite.findOne({
-            where: {
-                rating: req.params.rating
-            }
-        }).then(function (Campsite) {
-            res.json(Campsite);
-        });
+  app.get("/api/campsites/rating/:rating", function (req, res) {
+    Campsite.findOne({
+      where: {
+        rating: req.params.rating,
+      },
+    }).then(function (Campsite) {
+      res.json(Campsite);
     });
-    app.get("/api/campsites/location/:location", function (req, res) {
-        Campsite.findOne({
-            where: {
-                location: req.params.location
-            }
-        }).then(function (Campsite) {
-            res.json(Campsite);
-        });
+  });
+  app.get("/api/campsites/location/:location", function (req, res) {
+    Campsite.findOne({
+      where: {
+        location: req.params.location,
+      },
+    }).then(function (Campsite) {
+      res.json(Campsite);
     });
+  });
 
-
-
-
-
-    // route to post to campsites
-    app.post("/api/campsites", function (req, res) {
-        Campsite.create({
-            name: req.body.name,
-            body: req.body.body,
-            location: req.body.location
-        })
-            .then(function (Campsite) {
-                res.json(Campsite);
-            });
-
+  // route to post to campsites
+  app.post("/api/campsites", function (req, res) {
+    Campsite.create({
+      name: req.body.name,
+      body: req.body.body,
+      location: req.body.location,
+    }).then(function (Campsite) {
+      res.json(Campsite);
     });
+  });
 
-    // post route to wishlist campsite
-    app.post("/api/campsites/wishlist", function (req, res) {
-        Campsite.create({
-            name: req.body.name,
-            description: req.body.description,
-            location: req.body.location,
-            wishlist: req.body.wishlist
-        }).then(function (Campsite) {
-            res.json(Campsite);
-        });
+  // post route to wishlist campsite
+  app.post("/api/campsites/wishlist", function (req, res) {
+    Campsite.create({
+      name: req.body.name,
+      description: req.body.description,
+      location: req.body.location,
+      wishlist: req.body.wishlist,
+    }).then(function (Campsite) {
+      res.json(Campsite);
     });
+  });
 
-    app.delete("/api/campsites/:id", function (req, res) {
-        Campsite.destroy({
-            where: {
-                id: req.params.id
-            }
-        }).then(function (Campsite) {
-            res.json(Campsite);
-        });
+  app.delete("/api/campsites/:id", function (req, res) {
+    Campsite.destroy({
+      where: {
+        id: req.params.id,
+      },
+    }).then(function (Campsite) {
+      res.json(Campsite);
     });
+  });
 
-    app.put("/api/campsites", function (req, res) {
-        Campsite.update(req.body,
-            {
-                where: {
-                    id: req.body.id
-                }
-            }).then(function (Campsite) {
-                res.json(Campsite);
-            });
+  app.put("/api/campsites", function (req, res) {
+    Campsite.update(req.body, {
+      where: {
+        id: req.body.id,
+      },
+    }).then(function (Campsite) {
+      res.json(Campsite);
     });
-
+  });
 };
 
 // Campsite Name-STRING
